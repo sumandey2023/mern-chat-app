747fe9f748768d83f2ece74b2deffd1017ee9dd9
Suman Dey <140014618+sumandey2023@users.noreply.github.com>
2025-08-31 00:18:43 +0530

    refactor: Update AI model integration and add @google/genai dependency

--- backend/controller/ai.js
+++ backend/controller/ai.js
@@ -1,15 +1,20 @@
 const asyncHandler = require("express-async-handler");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI({});
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+// const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenAI } = require("@google/genai");
+const genAI = new GoogleGenAI({});
+// const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 const ai = asyncHandler(async (req, res) => {
   try {
     const { prompt } = req.body;
-    const result = await model.generateContent(prompt);
-    const text = result.response.text();
+    const result = await genAI.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: prompt,
+    });
+    const text = result.text;
     return res.status(200).json({ response: text });
   } catch (error) {
+    console.log(error);
     return res.status(400).json({ message: "Error processing AI response." });
   }
 });
